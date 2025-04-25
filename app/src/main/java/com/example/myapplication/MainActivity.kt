0fce0016e37c08c12d733e68fafc9c70be53c8d9
Egor <korpachev-2000@mail.ru>
2025-04-25 18:01:53 +0700

    Добавил всплывающее сообщение

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+private fun MainActivity.checkCharacter(string: String) {}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val textEdit = findViewById<EditText>(R.id.textEdit)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+
+        checkButton.setOnClickListener {
+            checkCharacter(textEdit.text.toString())
+        }
+        fun checkCharacter(input: String) {
+            val trimmedInput = input.trim()
+
+            if (trimmedInput.isEmpty()) {
+                Toast.makeText(this, "Введите символ", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            val ch = trimmedInput.first()
         }
     }
 }

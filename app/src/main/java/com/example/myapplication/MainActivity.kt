75600929c9109b8db5b535ba103fcd1c737dd19c
Egor <korpachev-2000@mail.ru>
2025-04-25 19:23:43 +0700

    Создана главаная активность приложения

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+class MainActivity : AppCompatActivity() {
+
+}

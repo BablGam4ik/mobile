1bd598eaea0009e978b1118d65aa10cc12ae706d
Egor <korpachev-2000@mail.ru>
2025-05-02 23:19:30 +0700

    Добавление UI элементов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputField = findViewById<EditText>(R.id.inputString)
+        val processButton = findViewById<Button>(R.id.processButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+        val modifiedText = findViewById<TextView>(R.id.modifiedText)
     }
 }

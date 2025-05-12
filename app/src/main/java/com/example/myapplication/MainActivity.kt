3d4db31d366a27a3e618592676920f5394cceaea
Egor <korpachev-2000@mail.ru>
2025-05-12 12:37:25 +0700

    Основная логика MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,25 @@
 package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+
 
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        calculateButton.setOnClickListener {
+            calculateCost()
+        }
+    }
+
+    private fun calculateCost() {
+
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
 }

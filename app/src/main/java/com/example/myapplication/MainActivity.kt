efe145a2e88f64ef0d7c450bd3e65269271373fa
Egor <korpachev-2000@mail.ru>
2025-05-14 22:37:08 +0700

    UI-фрагменты и FragmentManager

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,57 +1,24 @@
 package com.example.myapplication
-
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import kotlin.system.exitProcess
+
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState:
+                          Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        val textEdit = findViewById<EditText>(R.id.textEdit)
-        val checkButton = findViewById<Button>(R.id.checkButton)
-
-        checkButton.setOnClickListener {
-            checkCharacter(textEdit.text.toString())
-        }}
-        fun checkCharacter(input: String) {
-            val trimmedInput = input.trim()
-
-            if (trimmedInput.isEmpty()) {
-                Toast.makeText(this, "Введите символ", Toast.LENGTH_SHORT).show()
-                return
-            }
-
-            val ch = trimmedInput.first()
-
-            if (!ch.isLetter() || !isLatinLetter(ch)) {
-                Toast.makeText(this, "Символ не является латинской буквой", Toast.LENGTH_SHORT).show()
-                finish() // Завершение активности
-                exitProcess(0) // Полное завершение приложения (опционально)
-                return
-            }
-            when (ch.toLowerCase()) {
-                'a', 'e', 'i', 'o', 'u' -> Toast.makeText(this, "Это гласные буквы", Toast.LENGTH_SHORT).show()
-                else -> Toast.makeText(this, "Возможно, это согласные буквы", Toast.LENGTH_SHORT).show()
-            }
-
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container,
+                    fragment)
+                .commit()
         }
     }
+}
 
-    private fun isLatinLetter(ch: Char): Boolean {
-        return ch in 'A'..'Z' || ch in 'a'..'z'
-
-    }

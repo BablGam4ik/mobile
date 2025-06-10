825e6c3dd386384ef6a6667581f9f9327e8f2478
Egor <korpachev-2000@mail.ru>
2025-06-10 14:04:58 +0700

    Обновление версий плагинов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
@@ -40,7 +40,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    //implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
     // Lifecycle компоненты (заменяют устаревшие extensions)
